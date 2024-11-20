0f7ec33aea837cddecf97b83d86f3841431adfe4
DerbyNR <nr.derby@icloud.com>
2024-11-20 13:33:32 -0500

    Update part1.js

--- js/part1.js
+++ js/part1.js
@@ -4,7 +4,24 @@ document.getElementById("get-btn").addEventListener("click", async () => {
     // This function should send a GET request to the echo endpoint and output the result
     // The two input fields should be included in the request URL as **query parameters**
 
-    // TODO
+    // Get input data
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+
+    // Get URLSearchParams
+    const params = new URLSearchParams({name, age});
+    const url = `https://echo.zuplo.io/api?${params.toString()}`;
+
+    try {
+        // Send request
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // Display formatted json
+        output.textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+        output.textContent = 'Error';
+    }
 });
 
 document.getElementById("post-json-btn").addEventListener("click", async () => {
